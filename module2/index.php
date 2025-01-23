c961198f28e90f2e0a8b66c84c1616435b5b9686
olsaa0 <olsaolsa30@gmail.com>
2025-01-23 16:16:53 +0100

    variable

---
+++ module2/index.php
@@ -0,0 +1,26 @@
+<?php
+$emri="name";
+echo $emri;
+
+$number=5;
+echo $number;
+
+$sss=1.4;
+echo $sss;
+
+$bolean=true;
+echo $bolean;
+
+$x = 3;
+$y = 7;
+
+echo $x + $y;
+echo $x - $y;
+echo $x * $y;
+
+$fjali='olsa hihi yayy';
+echo strrev($fjali);
+echo strlen($fjali);
+echo str_word_count($fjali);
+echo str_replace('olsa hihi yayy', 'olsa hihii', $fjali);
+?>

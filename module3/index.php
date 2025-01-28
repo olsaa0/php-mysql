0899c2df19f1a2a7ba0ce6500b999379c7d8d1d0
olsaa0 <olsaolsa30@gmail.com>
2025-01-28 16:17:10 +0100

    if

---
+++ module3/index.php
@@ -0,0 +1,37 @@
+ <?php
+$point=100;
+if($point>=90){
+    echo '5';
+}
+elseif($point<90 && $point>=80){
+    echo '4';
+}
+elseif($point<90 && $point>=80){
+    echo '3';
+}
+elseif($point<90 && $point>=80){
+    echo '2';
+}
+elseif($point<90 && $point>=80){
+    echo '1';
+}  
+
+
+$day='monday';
+
+switch('$day'){
+    case 'monday':
+        echo 'nuk ki kurs';
+        break;
+        case 'tuesday':
+            echo 'ki kurs';
+            break;
+        case 'wednesday':
+            echo 'nuk ki kurs';
+                break;
+        case 'thursday':
+            echo 'ki kurs';
+            break;
+}
+
+?>

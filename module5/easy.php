20ab2b8d9b30ec35652fc193424a944676b49822
olsaa0 <olsaolsa30@gmail.com>
2025-02-13 15:54:55 +0100

    easy

---
+++ module5/easy.php
@@ -0,0 +1,19 @@
+<?php
+array(
+    "olsa" => "boss",
+    "person1" => "maid",
+    "person2" => "cook",
+    "person3" => "guard",
+);
+array(
+    "person4" => "boss",
+    "person5" => "maid",
+    "person6" => "cook",
+    "person7" => "guard",
+);
+array(
+    "person8" => "boss",
+    "person9" => "maid",
+    "person10" => "cook",
+    "person11" => "guard",
+);

40d04ee673642c4f5a0d9639d71aa516e9d40bc0
olsaa0 <olsaolsa30@gmail.com>
2025-03-20 16:17:17 +0100

    deletee

--- module11/delete.php
+++ module11/delete.php
@@ -1,6 +1,9 @@
 <?php
-include_once('update.php');
+include_once('config.php');
 $id = $_GET['id'];
+$prep = $connection->prepare($sql);
+
+ $prep->bindParam(':id' ,$id);
 $sql = "DELETE FROM users WHERE id=:id;";
 $sql->execute();
 ?>
